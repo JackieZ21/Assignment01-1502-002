821cd4299aef27d019e610dec9756250566c97f1
Jackie Zheng <j4ckmanz14@gmail.com>
2022-01-27 23:20:58 -0700

    Here is the second commit for player class that has the java doc information

--- Assignment 1 - Skeleton/src/mru/game/model/Player.java
+++ Assignment 1 - Skeleton/src/mru/game/model/Player.java
@@ -16,24 +16,58 @@ public class Player {
 		this.numOfWins=numOfWins;
 		
 	}
+	/**
+	 * this method sets the name for the player 
+	 * @param name
+	 */
 	public void setName(String name) {
 		this.name=name;
+		//takes the most local variable name and saves it into name
 	}
+	/**
+	 * this method returns the name of the player
+	 * @return name
+	 */
 	public String getName() {
 		return name;
+		//return the player name
 	}
+	/**
+	 * this method sets the player id number in
+	 * @param id
+	 */
 	public void setId(String id) {
 		this.id=id;
+		//takes the most local variable of id and saves it into id 
 	}
+	/**
+	 * this method returns the player id number
+	 * @return id number 
+	 */
 	public String getId() {
 		return id;
+		//return id number
+		
 	}
+	/**
+	 * this method set the number of wins in the variable
+	 * @param numOfWins
+	 */
 	public void setNumOfWins(int numOfWins) {
 		this.numOfWins=numOfWins;
+		//set the most local variable of numOfWins in the numOfWins
 	}
+	/**
+	 * this method returns the number of wins the player has
+	 * @return number of wins
+	 */
 	public int getNumOfWins() {
 		return numOfWins;
+		//return the number of wins for the corresponding player
 	}
+	/**
+	 * this method returns the players informations such as name id and number of wins
+	 */
 	public String toString() {
 		return "Name: "+name+"ID: "+ id+ "Number of Wins: "+ numOfWins;
 	}
